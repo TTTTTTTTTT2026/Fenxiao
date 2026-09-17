7955b3e3eeb1b011bd8859e350288df2504c60aa
Eastion <ycshen1990@gmail.com>
2026-09-17 16:29:07 +0800

    fix(grade): import nonnegative point validation

--- src/main/java/com/fenxiao/incentive/dto/UserGradeLevelRequest.java
+++ src/main/java/com/fenxiao/incentive/dto/UserGradeLevelRequest.java
@@ -3,6 +3,7 @@ package com.fenxiao.incentive.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
